5ca336373937485826ade9943dec72ebdab61980
MuwaEric <muwanikaeric602@gmail.com>
2024-10-05 14:20:28 +0300

    first commit

---
+++ src/main/java/com/example/agri_App/AgriApp.java
@@ -0,0 +1,18 @@
+package com.example.agri_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.agri_App", "com.example.productcatalog"})
+@EntityScan(basePackages = "com.example.productcatalog.model")
+@EnableJpaRepositories(basePackages = "com.example.productcatalog.repository")
+public class AgriApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriApp.class, args);
+    }
+}
---
+++ src/test/java/com/example/AgriApp/AgriAppTests.java
@@ -0,0 +1,13 @@
+package com.example.agri_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
